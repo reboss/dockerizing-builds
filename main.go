e4e249bd06c5e29aec4aff159b247c3653769026
John Robbins <j.reboss@gmail.com>
2023-04-03 15:16:46 -0600

    Initialize go project

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
     engine := gin.Default()
 
-    engine.Get("/", func(c *gin.Context) {
+    engine.GET("/", func(c *gin.Context) {
         c.JSON(http.StatusOK, gin.H{
             "message": "Hello, world!",
         })
